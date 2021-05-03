77033dbd5f96fbbce6546bffbd61f46ec51b466c
Suzy <suzy.alvesmano@gmail.com>
2021-05-03 19:05:27 +0200

    menu files

--- doc/html_test/app.js
+++ doc/html_test/app.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
 
+
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
@@ -9,7 +10,32 @@ const navSlide = () => {
     burger.classList.toggle('toggle');
     });
 
-    
 }
 
 navSlide();
+
+const contentSlide = () => {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(function(button) {
+        var contents = buttons.parentElement.getElementsByClassName('content');
+
+        contents.forEach(button => {
+            button.addEventListener('click', () => {
+                console.log(content.classList);
+                content.classList.toggle('content-active');
+                content.classList.toggle('content');
+                console.log(content.classList);
+            });
+        });
+        /* .forEach(function(content){
+            
+                
+            }); 
+        });*/
+
+       
+    });
+    
+}
+
+contentSlide();
