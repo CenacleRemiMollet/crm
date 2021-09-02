426618b1b57e542a350ba98ce519b797e8db61c7
Fabrice Agu <f.agu@oodrive.com>
2021-09-02 10:12:43 +0200

    Add club infos

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -44,4 +44,25 @@ class ClubController extends AbstractController
 		]);
 	}
 
+	/**
+	 * @Route("/club/{uuid}/infos", name="web_club_infos", methods={"GET"}, requirements={"uuid"="[a-z0-9_]{2,64}"})
+	 */
+	public function viewInfos($uuid, LoggerInterface $logger, SessionInterface $session)
+	{
+		$response = $this->forward('App\Controller\Api\ClubController::one', ['uuid' => $uuid]);
+		if($response->getStatusCode() != 200) {
+			return $this->render('club/club-not-found.html.twig', []);
+		}
+		$club = json_decode($response->getContent());
+		$session->set('club-selected', $club);
+
+		$response = $this->forward('App\Controller\Api\ClubController::getLessons', ['uuid' => $uuid]);
+		$lessons = json_decode($response->getContent());
+		$session->set('lessons-selected', $lessons);
+
+		return $this->render('club/club-infos.html.twig', [
+			'club' => $club,
+			'lessons' => $lessons
+		]);
+	}
 }
