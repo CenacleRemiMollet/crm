c69a3701408b371d6226348e61491881bb18b43e
fagu <fagu@free.fr>
2022-02-19 14:47:06 +0100

    save

--- doc/sql/cities.sql
+++ doc/sql/cities.sql
@@ -7,14 +7,14 @@ LOAD DATA LOCAL INFILE 'doc/sql/eucircos.csv'
  (@dummy, @dummy, @dummy, @dummy, county_number, @dummy, @dummy, @dummy, city_name, zip_code, @dummy, latitude, longitude, @dummy);
 
 
-DELETE FROM city
- WHERE id IN (
-  SELECT city.id
-   FROM city
-    LEFT OUTER JOIN (
-    SELECT min(id) AS id, zip_code, city_name
-      FROM city
-      GROUP BY zip_code, city_name
-    ) AS uniq ON city.id = uniq.id
-   WHERE uniq.id IS NULL
- );
+-- DELETE FROM city
+--  WHERE id IN (
+--   SELECT city.id
+--    FROM city
+--     LEFT OUTER JOIN (
+--     SELECT min(id) AS id, zip_code, city_name
+--       FROM city
+--       GROUP BY zip_code, city_name
+--     ) AS uniq ON city.id = uniq.id
+--    WHERE uniq.id IS NULL
+--  );
